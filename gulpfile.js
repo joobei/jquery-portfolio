c77a471080d5850d4e7edcaec9c3b5a408d929d2
Arsene Toumani <newrezgaming@gmail.com>
2014-04-22 20:19:03 -0400

    added dist

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,20 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var minifyCSS = require('gulp-minify-css');
+var concat = require('gulp-concat');
 
 // Uglify js
 gulp.task('scripts', function() {
     return gulp.src('demo/*.js')
-        .pipe(uglify({outSourceMap: true}))
+        .pipe(concat('portfolio.jquery.min.js'))
+        .pipe(uglify())
         .pipe(gulp.dest('dist'));
 });
 
 // Minify css
 gulp.task('styles', function() {
     return gulp.src('demo/*.css')
+        .pipe(concat('portfolio.jquery.min.css'))
         .pipe(minifyCSS())
         .pipe(gulp.dest('dist'));
 });
